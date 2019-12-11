ae72804fe67231b6a4c64bbef8373d483d355f9e
Scott Renz <scottrenz@scottrenz.com>
2019-12-11 02:27:13 -0500

    made m with calculation to prevent overflow

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,5 +1,6 @@
 import sys
-sys.setrecursionlimit(3000)
+#sys.setrecursionlimit(3000)
+#print(sys.getrecursionlimit())
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     #elements = len( arrA ) + len( arrB )
@@ -44,7 +45,7 @@ def merge_sort( arr ):
     right =merge_sort(right)
     arr = merge(left,right)
     return arr
-print(merge_sort([99,8,3,88,1,0,4,5,7,43,9,2]))
+#print(merge_sort([99,8,3,88,1,0,4,5,7,43,9,2]))
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -72,14 +73,15 @@ def merge_in_place(arr, start, mid, end):
 def merge_sort_in_place(arr, l, r): 
     # TO-DO
     if l < r:
-        m = (l + r) // 2
-#        m = l + (r -1) // 2
+        # m = (l + r) // 2
+        m = ( l + (r -1) )// 2
         merge_sort_in_place(arr, l, m)
         merge_sort_in_place(arr, m + 1, r)
         merge_in_place(arr, l, m, r)
     return arr
-#print(merge_sort_in_place([99,8,9,1,2], 0, 4))
-#print(merge_sort_in_place([99,8,3,88,1,0,4,5,7,43,9,2], 0, 11))
+# print(merge_sort_in_place([99,8,9,1,2], 0, 4))
+# print(merge_sort_in_place([99,8,3,88,1,0,4,5,7,43,9,2], 0, 11))
+# print(merge_sort_in_place(['xz','ax','d','tree','m'], 0, 4))
 
 
 # STRETCH: implement the Timsort function below
